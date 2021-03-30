3668a81beda724ae3924cce886f3edd5ffabe282
Mitchell Knollenberg <mitchell.knollenberg@gmail.com>
2021-03-30 17:00:09 -0500

    added google log on

--- app/src/main/java/com/example/courseconnection/Home.java
+++ app/src/main/java/com/example/courseconnection/Home.java
@@ -1,14 +1,74 @@
 package com.example.courseconnection;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class Home extends AppCompatActivity {
+
+    TextView txt;
+    Button signOutBtn;
+    GoogleSignInClient mGoogleSignInClient;
+    Button unlinkBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        txt = (TextView)findViewById(R.id.textView);
+        signOutBtn = (Button)findViewById(R.id.signOutBtn);
+        unlinkBtn = (Button)findViewById(R.id.unlinkBtn);
+
+        String email = getIntent().getStringExtra("EMAIL");
+        txt.setText("You are currently signed in as: " + email);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mGoogleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
+            }
+        });
+
+        unlinkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                revokeAccess();
+            }
+        });
     }
+
+    private void revokeAccess() {
+        mGoogleSignInClient.revokeAccess()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
+    }
+
 }
--- app/src/main/java/com/example/courseconnection/MainActivity.java
+++ app/src/main/java/com/example/courseconnection/MainActivity.java
@@ -5,42 +5,118 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 101;
+    private static final String TAG = "SignInActivity";
+    private View.OnClickListener listener;
 
     EditText email,password;
     Button loginBtn;
     TextView invalidCreds;
+    GoogleSignInClient mGoogleSignInClient;
+    SignInButton signInBtn;
+    GoogleSignInAccount account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        email = (EditText)findViewById(R.id.email);
-        password = (EditText)findViewById(R.id.password);
-        invalidCreds =(TextView)findViewById(R.id.invalidCreds);
-        loginBtn = (Button)findViewById(R.id.loginBtn);
-
-        loginBtn.setOnClickListener(new View.OnClickListener() {
+        listener = new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                if (email.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
-                    //correct password
-                    invalidCreds.setVisibility(View.INVISIBLE);
-                    Intent intent = new Intent(getApplicationContext(),Home.class);
-                    startActivity(intent);
-                } else {
-                    //wrong password
-                    invalidCreds.setVisibility(View.VISIBLE);
+                switch (v.getId()) {
+                    // user clicked the google sign in button
+                    case R.id.sign_in_button:
+                        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                        startActivityForResult(signInIntent, RC_SIGN_IN);
+                        break;
+                    // user clicked other sign in button
+                    case R.id.loginBtn:
+                        if (email.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                            //correct password
+                            signIn();
+                        } else {
+                            //wrong password
+                            invalidCreds.setVisibility(View.VISIBLE);
+                        }
                 }
             }
-        });
+        };
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        email = (EditText)findViewById(R.id.email);
+        password = (EditText)findViewById(R.id.password);
+        invalidCreds = (TextView)findViewById(R.id.invalidCreds);
+        loginBtn = (Button)findViewById(R.id.loginBtn);
+        signInBtn = (SignInButton)findViewById(R.id.sign_in_button);
+
+        signInBtn.setOnClickListener(listener);
+
+        loginBtn.setOnClickListener(listener);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account != null){
+            signIn();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            account = completedTask.getResult(ApiException.class);
+            // Signed in successfully, show authenticated UI.
+            signIn();
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            invalidCreds.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void signIn(){
+        invalidCreds.setVisibility(View.INVISIBLE);
+        Intent intent = new Intent(getApplicationContext(),Home.class);
+        intent.putExtra("EMAIL",account.getEmail());
+        startActivity(intent);
+    }
 }
