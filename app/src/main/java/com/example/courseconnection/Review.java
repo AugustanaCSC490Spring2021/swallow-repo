ac27e87f105dd7e290c8cecb0b346218c27f41bb
Palmer5465 <jasonpalmer17@augustana.edu>
2021-04-21 20:42:43 -0500

    Course reviews can now be seen in the review fragment.

--- app/src/main/java/com/example/courseconnection/Review.java
+++ app/src/main/java/com/example/courseconnection/Review.java
@@ -3,18 +3,29 @@ package com.example.courseconnection;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,8 +40,12 @@ public class Review extends Fragment implements AdapterView.OnItemSelectedListen
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "";
     private Spinner deptSpinner;
     private Button addReviewBtn;
+    private ListView lvCourses;
+    private List<String> courses = new ArrayList<>();
+    private ArrayAdapter listAdapter;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -71,6 +86,7 @@ public class Review extends Fragment implements AdapterView.OnItemSelectedListen
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        courses.clear();
         View view = inflater.inflate(R.layout.fragment_review, container, false);
         deptSpinner = (Spinner)view.findViewById(R.id.departmentSpinner);
         addReviewBtn = (Button)view.findViewById(R.id.addReviewBtn);
@@ -78,8 +94,38 @@ public class Review extends Fragment implements AdapterView.OnItemSelectedListen
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         deptSpinner.setAdapter(adapter);
         deptSpinner.setOnItemSelectedListener(this);
+        lvCourses = view.findViewById(R.id.lvCourses);
+        listAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, courses);
+        lvCourses.setAdapter(listAdapter);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("reviews")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            String course = new String();
+                            String score = new String();
+                            String comment = new String();
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                course = (String) document.get("course");
+                                score = document.get("rating").toString();
+                                comment = (String) document.get("comment");
+                                course = course +": "+score+" stars\n\""+comment+"\"";
+                                courses.add(course);
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                        listAdapter.notifyDataSetChanged();
+                    }
+                });
 
         addReviewBtn.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), LeaveAReview.class);
